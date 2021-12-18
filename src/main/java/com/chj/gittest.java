16b7a4a3f996cac46df6af19f5402f483a3d4e33
chj_global <1648888_global>
2021-12-18 10:27:56 +0800

    third commit

--- src/main/java/com/chj/gittest.java
+++ src/main/java/com/chj/gittest.java
@@ -8,5 +8,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hellogit");
         System.out.println("hello gotjhhh");
+        System.out.println("hello gotjjjj");
     }
 }

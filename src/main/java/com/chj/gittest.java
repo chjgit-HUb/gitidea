598036397a257dcd92738795a3fecf66b8a1813a
chj_global <1648888_global>
2021-12-18 10:25:10 +0800

    first commit

---
+++ src/main/java/com/chj/gittest.java
@@ -0,0 +1,11 @@
+package com.chj;
+
+/**
+ * @author chj
+ * @create 2021-12-18-10:21
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hellogit");
+    }
+}
